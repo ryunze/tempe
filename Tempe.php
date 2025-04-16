92c04cd304182d5f132f57cfa3bc87e7998f91ff
ryunze <ryunzeroeight08@gmail.com>
2025-04-16 17:32:50 +0700

    Create Tempe.php

---
+++ Tempe.php
@@ -0,0 +1,34 @@
+<?php
+
+class Tempe {
+
+    public $section = [];
+    public $currentSection;
+    public $extendView;
+
+    public function render($s)
+    {
+        return $this->section[$s];
+    }
+
+    public function section($s)
+    {
+        $this->currentSection = $s;
+        ob_start();
+        ob_flush();
+    }
+
+    public function endsection()
+    {
+        $currentSection = $this->currentSection;
+        $this->section[$currentSection] = ob_get_flush();
+        ob_end_clean();
+        include_once(__DIR__ . '/' . $this->extendView . '.php');
+    }
+
+    public function extend($f)
+    {
+        $this->extendView = $f;
+    }
+
+}
